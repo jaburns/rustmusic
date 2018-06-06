b209620c48fb0b400d9f5934bd6dbe0558090973
Jeremy Burns <j@jaburns.net>
2018-06-05 19:53:10 -0600

    Implemented sine tone audio device

--- src/dsp.rs
+++ src/dsp.rs
@@ -3,6 +3,7 @@ use sdl2::{AudioSubsystem};
 use std::path::Path;
 use std::sync::mpsc::{Receiver};
 use std::mem;
+use std::f32::consts::{PI};
 
 pub struct Sound {
     rx: Receiver<bool>,
@@ -26,13 +27,76 @@ impl AudioCallback for Sound {
     }
 }
 
+trait Producer {
+    fn write_samples(&mut self, buffer: &mut [f32]);
+}
+
+pub struct Sine {
+    freq_over_sample_freq: f32,
+    offset: f32,
+}
+
+impl Sine {
+    pub fn new(sample_freq: u32, freq: f32) -> Sine {
+        Sine {
+            freq_over_sample_freq: freq / (sample_freq as f32),
+            offset: 0f32,
+        }
+    }
+}
+
+impl Producer for Sine {
+    fn write_samples(&mut self, buffer: &mut [f32]) {
+        for i in 0..buffer.len() {
+            let t = self.offset + (i as f32);
+            buffer[i] = f32::sin(2f32 * PI * self.freq_over_sample_freq * t);
+        }
+        self.offset += buffer.len() as f32;
+    }
+}
+
+struct StereoSplit<T: Producer> { 
+    source: T
+}
+
+impl<T: Producer> StereoSplit<T> {
+    pub fn new(source: T) -> StereoSplit<T> {
+        StereoSplit {
+            source: source
+        }
+    }
+}
+
+impl<T: Producer> Producer for StereoSplit<T> {
+    fn write_samples(&mut self, buffer: &mut [f32]) {
+        let mut half_vec = vec![0f32; buffer.len() / 2];
+        self.source.write_samples(&mut half_vec);
+
+        for i in 0..buffer.len() {
+            buffer[i] = half_vec[i / 2];
+        }
+    }
+}
+
+pub struct DDD {
+    prod: StereoSplit<Sine>
+}
+
+impl AudioCallback for DDD {
+    type Channel = f32;
+
+    fn callback(&mut self, out: &mut [f32]) {
+        self.prod.write_samples(out);
+    }
+}
+
 fn as_floats(v: &[u8]) -> Vec<f32> {
     v.chunks(4)
         .map(|s: &[u8]| unsafe { mem::transmute([s[0], s[1], s[2], s[3]]) })
         .collect()
 }
 
-pub fn create_device(audio: AudioSubsystem, rx: Receiver<bool>) -> AudioDevice<Sound> {
+pub fn create_device(audio: AudioSubsystem, rx: Receiver<bool>) -> AudioDevice<DDD> {
     let desired_spec = AudioSpecDesired {
         freq: Some(44_100),
         channels: Some(2),
@@ -55,12 +119,18 @@ pub fn create_device(audio: AudioSubsystem, rx: Receiver<bool>) -> AudioDevice<S
 
             let data_bytes: Vec<u8> = cvt.convert(wav.buffer().to_vec());
 
-            Sound {
-                rx: rx,
-                data: as_floats(data_bytes.as_slice()),
-                volume: 1.00,
-                pos: 1_000_000,
+            let mut stereo = StereoSplit::<Sine>::new(Sine::new(44_100, 440f32));
+
+            DDD {
+                prod: stereo
             }
+
+        //  Sound {
+        //      rx: rx,
+        //      data: as_floats(data_bytes.as_slice()),
+        //      volume: 1.00,
+        //      pos: 1_000_000,
+        //  }
         })
         .unwrap();
 
